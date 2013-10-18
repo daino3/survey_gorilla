74aadd60ce4c1de01959f23ec7699d48d45b0d51
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 14:14:36 -0500

    added jquery choices

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,6 +9,7 @@ get '/create_survey' do
 end
 
 post '/create_survey' do
+  puts params
   @survey = Survey.create(title: params[:survey_title]) #, user_id: session[:user_id])
   
   if params[:image] 
@@ -17,4 +18,20 @@ post '/create_survey' do
   end
 
   erb :create_question
+end
+
+get '/create_question' do
+  erb :create_question
+end
+
+post '/create_question' do
+  @survey = Survey.find(params[:survey])
+  @question = Question.create(prompt: params[:question], survey_id: @survey.id)
+  create_choices(params[:choice_input], @question.id)
+
+  if params[:submit] == "Finish"
+    erb :view_survey
+  elsif params[:submit] == "Add Question"
+    erb :create_question
+  end
 end
--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,3 +1,9 @@
 def current_user
   @current_user = User.find(session[:user_id]) if session[:user_id]
 end
+
+def create_choices(choices_hash, question_id)
+  choices_hash.each do |key, value|
+    Choice.create(option: value, question_id: question_id)
+  end
+end
