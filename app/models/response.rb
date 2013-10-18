1a753dae31631b3fa2ff98090b78672e23397162
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-18 14:19:07 -0500

    pulling in from master 2:19

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,6 @@
 class Response < ActiveRecord::Base
   # Remember to create a migration!
     belongs_to :choice
-    belongs_to :user, through: :takensurveys
+    belongs_to :takensurvey
+    belongs_to :user, through: :takensurvey
 end
