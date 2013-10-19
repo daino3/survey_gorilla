e9757789cc16986f05d732320d5d11126da063ed
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-19 16:15:10 -0500

    update survey done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -82,13 +82,26 @@ get '/view_all_surveys' do
   erb :view_all_surveys
 end
 
-get '/edit_survey/:survey_id' do 
+get '/edit_survey/:survey_id' do
   @survey = Survey.find(params[:survey_id])
-  erb :edit_survey
+  if current_user.id == @survey.creator_id
+    erb :edit_survey
+  else
+    redirect '/'
+  end
 end
 
 post '/edit_survey' do
-  p params
+  @survey = Survey.find(params[:survey])
+  @survey.update_attributes(title: params[:survey_title])
+  if params[:image] 
+    @photo = Photo.create(title: params[:image_title], file: params[:image])
+    @survey.photo = @photo
+  else
+    @photo = @survey.photo
+  end
+  @photo.update_attributes(title: params[:image_title])
+  redirect "/edit_survey/#{@survey.id}"
 end
 
 post '/delete_question' do
